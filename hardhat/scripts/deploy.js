185607b5bbe1553116ad0a057ce05287b47ce84a
Kimonic <aimuaimarengiaye@gmail.com>
2023-02-19 14:11:40 -0500

    first commit

---
+++ hardhat/scripts/deploy.js
@@ -0,0 +1,37 @@
+// We require the Hardhat Runtime Environment explicitly here. This is optional
+// but useful for running the script in a standalone fashion through `node <script>`.
+//
+// You can also run a script with `npx hardhat run <script>`. If you do that, Hardhat
+// will compile your contracts, add the Hardhat Runtime Environment's members to the
+// global scope, and execute the script.
+const hre = require("hardhat");
+
+// const { ethers } = require("hardhat");
+require("dotenv").config({ path: ".env" });
+const { NAIJA_DEV_TOKEN_CONTRACT_ADDRESS } = require("../constants");
+
+async function main() {
+  const naijaDevTokenAddress = NAIJA_DEV_TOKEN_CONTRACT_ADDRESS;
+  /*
+  A ContractFactory in ethers.js is an abstraction used to deploy new smart contracts,
+  so exchangeContract here is a factory for instances of our Exchange contract.
+  */
+  const exchangeContract = await ethers.getContractFactory("Exchange");
+
+  // here we deploy the contract
+  const deployedExchangeContract = await exchangeContract.deploy(
+    naijaDevTokenAddress
+  );
+  await deployedExchangeContract.deployed();
+
+  // print the address of the deployed contract
+  console.log("Exchange Contract Address:", deployedExchangeContract.address);
+}
+
+// Call the main function and catch if there is any error
+main()
+  .then(() => process.exit(0))
+  .catch((error) => {
+    console.error(error);
+    process.exit(1);
+  });
